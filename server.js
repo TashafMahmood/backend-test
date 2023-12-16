ab6be51a249d487be662e2d331b334eb4bd5df68
TashafMahmood <tashaf@elred.io>
2023-12-17 03:38:06 +0530

    changes

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const PORT = 5000
 
-const app = express()
+const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("WELCOME TO BACKEND LEARNING")
-})
+app.get("/", (req, res) => {
+  res.send("WELCOME TO BACKEND LEARNING");
+});
 
-app.listen(5000, ()=>{
-    console.log("SERVER STARTED SUCCESSFULLY");
-})
+app.get("/getusers", (req, res) => {
+  res.status(200).json([
+    { id: 1, name: "tashaf mahmood" },
+    { id: 2, name: "meezan nafees" },
+    { id: 3, name: "rehan shakeel" },
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log("SERVER STARTED SUCCESSFULLY");
+});
